b8424e1f97e10496bd48b68addff215240bf37e8
FaridIzquierdo <u202015215@upc.edu.pe>
2022-09-30 20:34:26 -0500

    build:Home-Message

--- src/components/public/Home.vue
+++ src/components/public/Home.vue
@@ -6,13 +6,13 @@
       <div class="box_blank2 p-4 shadow-0 border-round w-auto lg:w-100 Home"> </div>
       <aside class="text3 font-medium line-height-3 ">Popular Stores</aside>
       <div class="box3 p-4 shadow-0 border-round w-auto lg:w-20">
-        <a href="https://www.walmart.com/" target="_blank"><img class="Walmart" alt="Walmart" src="src/components/img/Walmart.jpg"></a>
-        <a href="https://www.amazon.com/" target="_blank"><img class="Amazon" alt="Amazon" src="src/components/img/Amazon.png"></a>
+        <a href="" target="_blank"><img class="Walmart" alt="Walmart" src=""></a>
+        <a href="" target="_blank"><img class="Amazon" alt="Amazon" src=""></a>
       </div>
       <div class="box6 p-4 shadow-0 border-round w-full lg:w-20"></div>
       <div class="box4 p-4 shadow-0 border-round w-auto lg:w-20">
-        <a href="https://www.ebay.com/" target="_blank"><img class="Ebay" alt="Ebay" src="src/components/img/Ebay.png"></a>
-        <a href="https://www.apple.com/" target="_blank"><img class="Apple" alt="Apple" src="src/components/img/Apple.png"></a>
+        <a href="" target="_blank"><img class="Ebay" alt="Ebay" src=""></a>
+        <a href="" target="_blank"><img class="Apple" alt="Apple" src=""></a>
       </div>
     </div>
 
@@ -23,20 +23,20 @@
       <aside class="text3 font-medium line-height-3">Recent Orders</aside>
       <div class="Boxes">
         <div class="box1 p-4 shadow-1 border-round w-full lg:w-20">
-          <img id="PhotoIphone" alt="PhotoIphone" src="src/components/img/Iphone.png">
+          <img id="PhotoIphone" alt="PhotoIphone" src="">
           <center>
             <aside class="Subtitle1">Iphone 13 PRO MAX</aside>
             <aside class="time1">3 months ago</aside>
-            <img class="Apple2" alt="Apple2" src="src/components/img/Apple.png">
+            <img class="Apple2" alt="Apple2" src="">
           </center>
         </div>
         <div class="box7 p-4 shadow-0 border-round w-full lg:w-20"></div>
         <div class="box2 p-4 shadow-1 border-round w-full lg:w-20">
-          <img id="PhotoXbox" alt="PhotoXbox" src="src/components/img/Xbox.png">
+          <img id="PhotoXbox" alt="PhotoXbox" src="">
           <center>
             <aside class="Subtitle2">XBOX SERIES S 512GB</aside>
             <aside class="time2">5 months ago</aside>
-            <img class="Amazon2" alt="Amazon2" src="src/components/img/Amazon2.png">
+            <img class="Amazon2" alt="Amazon2" src="">
           </center>
         </div>
       </div>
