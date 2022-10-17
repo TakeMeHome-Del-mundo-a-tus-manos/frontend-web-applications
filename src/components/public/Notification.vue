65ad88d96f7ea821bd0126d1019b822d096b9956
FaridIzquierdo <u202015215@upc.edu.pe>
2022-10-17 00:50:32 -0500

    feature/support-notification
    ...

--- src/components/public/Notification.vue
+++ src/components/public/Notification.vue
@@ -10,8 +10,11 @@ onClickOutside(modal,()=>(OpenModal.value=false))
 export default {
   data() {
     return {
-      OpenModal: true,
-      modal:null
+      OpenModalMain: true,
+      modal:null,
+      //borrar
+      OpenModal2: false,
+      modal2:null
     }
   }
 }
@@ -20,14 +23,22 @@ export default {
 <template>
 
 
-  <div v-if="OpenModal" class="modal-bg">
+  <div v-if="OpenModalMain" class="modal-bg">
     <div class="modal" ref="modal">
-      <button @click="OpenModal = false" class="close-btn">x</button>
+      <button @click="OpenModalMain = false" class="close-btn">x</button>
       <div class="not1-box">
         <img class="not-message" src="src/components/img/not-message.png" alt="message">
         <div class="not-resume">
           <h3 class="not-resume-txt">You have a new order.</h3>
           <h3 class="not-resume-txt">Click to read the contract</h3>
+
+
+          <button @click="OpenModal2 = true" class="OpenNewModal">new</button>
+          <div v-if="OpenModal2" class="modal2-bg">
+            <div class="modal2" ref="modal2">
+            </div>
+          </div>
+
         </div>
       </div>
       <div class="not2-box">
@@ -62,11 +73,34 @@ export default {
   justify-content:center;
   align-items: center;
 }
+.modal2-bg{
+  position:fixed;
+  top:0;
+  left:0;
+  width: 100vw;
+  height: 100vh;
+
+  background-color:rgba(0,0,0,0.5);
+
+  display :flex;
+  justify-content:center;
+  align-items: center;
+}
 .modal{
   position:relative;
-  background: white;
+  background: rgba(255, 255, 255, 0.92);
   padding:50px 100px;
-  border-radius: 5px;
+  border-radius: 7px;
+  box-shadow: 0 10px 5px 2px rgba(0,0,0,0.1);
+  width: 580px;
+  margin-top: -420px;
+  margin-right: -520px;
+}
+.modal2{
+  position:relative;
+  background: rgba(255, 255, 255, 0.92);
+  padding:50px 100px;
+  border-radius: 7px;
   box-shadow: 0 10px 5px 2px rgba(0,0,0,0.1);
   width: 580px;
   margin-top: -420px;
@@ -78,12 +112,13 @@ export default {
   left:465px;
 }
 .not1-box{
-  box-shadow: 0px 0px 1px black;
   width: 500px;
   height: 100px;
   top:100px;
   margin-top: -60px;
   margin-left: -60px;
+  background: white;
+  border-radius: 7px;
 }
 .not-resume{
   width:280px;
@@ -101,11 +136,12 @@ export default {
   width:400px;
 }
 .not2-box{
-  box-shadow: 0px 0px 1px black;
   width: 500px;
   height: 100px;
   top:100px;
   margin-top: 15px;
   margin-left: -60px;
+  background: white;
+  border-radius: 7px;
 }
 </style>
