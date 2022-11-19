e1f8af2efcb7e915f70c4955f0b6015439ade4e4
JennyBalceda <89040419+JennyBalceda@users.noreply.github.com>
2022-11-18 21:49:03 -0500

    Update MyPurchases.vue
    My purchases implentation using api web services

--- src/components/private/resume/resume-my-purchases/MyPurchases.vue
+++ src/components/private/resume/resume-my-purchases/MyPurchases.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="grid">
     
-        <div class="col-6">
-          <h2>My Purchase History</h2>
+      <div class="col-6">
+        <h2>My Purchase History</h2>
           <div class="Boxes" v-for="product in recent_orders.slice(0,2)" :key="product.id"> 
-            <div class="box1 p-4 shadow-1 border-round w-full lg:col-6">
-              <img class="home-img" id="PhotoIphone" alt="PhotoIphone" :src="product.productImage">
+            <div class="box1 p-4 shadow-1 border-round w-full lg:w-20">
+              <img id="Iphone" alt="product image" :src="product.productUrl">
               <center>
-                <aside class="Subtitle1">{{product.productName}}</aside>
-                <aside class="time1">{{product.requestDate.slice(0, 10)}}</aside>
+                <aside class="Subtitle1">{{product.name}}</aside>
+                <aside class="time1">{{product.order.deadlineDate}}</aside>
                 <img class="Apple2 home-img" alt="Apple2" :src="product.productStore">
               </center>
             </div>
           </div>
         </div>
-    
+      </div>
     </div>
    
 </template>
@@ -34,17 +34,12 @@ export default {
     }
   },
   created(){ 
-
-    this.purchaseUserApiService = new UserApiService()
     
-    this.purchaseUserApiService.getRecentOrders(localStorage.getItem("id")).then((response) => {
-      this.recent_orders = response.data;
-     
-    });
-  },
-  methods: {
+    this.homeUserApiService = new UserApiService()
 
+    this.homeUserApiService.getRecentOrders(localStorage.getItem("id")).then((response) => {this.recent_orders=response.data; console.log(this.recent_orders)});
   }
+  
 
 }
 </script>
